f24c19bed355d18006f00281a99a308ba5de568f
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-03-27 23:21:17 +0530

    this code explains about the arithmetic operators

---
+++ Operators.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+add = a + b #addition of the two operands
+sub = a - b #subtraction of two operands
+multi = a * b #multiplication of two operands
+div = a / b #division of two operands which gives the float value of the quotient
+floordiv = a // b #division of two operands gives the integer value of the quotient
+modulus = a % b #division of two operands gives the remainder
+power = a ** b #gives the result of power of the left operand to the right operand
+
+print("addition: ",add)
+print("subtraction: ", sub)
+print("multiplication: ", multi)
+print("division: ", div)
+print("floor division: ", floordiv)
+print("modulus: ", modulus)
+print("power: ", power)
