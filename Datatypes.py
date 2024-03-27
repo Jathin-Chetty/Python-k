48b2e55b7062d2e9267d4dd6b36358072dbeb592
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-03-27 14:40:23 +0530

    this code contains the basic data types used in python

---
+++ Datatypes.py
@@ -0,0 +1,22 @@
+#integer data type
+a = 10
+print(a)
+#float data type
+b = 3.14
+print(b)
+#string data type
+c = "String"
+print(c)
+#boolean data type
+d = True #it can also be false
+print(d)
+#set data type which is a collection of key-value pairs
+e = {1, 2, 3}
+print(e)
+
+#type() is function is used to check the data type of the variable 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
