aa5945470f35064dc7c263de0a8081b974f79892
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-03-28 23:31:41 +0530

    these files contain the comparision, logical operators

---
+++ Comparision-and-logical-operators.py
@@ -0,0 +1,9 @@
+#comparision operators
+a = 10
+b = 5
+print(a == b) # result will be true if both variables are equal else false
+print(a != b) #result will be false if variables are not equal else true
+print(a > b) #result will be true if left variable is greater than right variable else false
+print(a < b) #result will be true if left variable is less than right variable else false
+print(a <= b) #result will be true if left variable is less than or equal to right variable else false
+print(a >= b) #result will be true if left variable is greater than or equal to right variable else false
---
+++ Logical-operators.py
@@ -0,0 +1,10 @@
+#Logical operators
+a = 10
+b = 5
+#Logical and operator-the result of the 'and' statement will be true if both statements will evaluate to true else it will result to false
+print((a > b) and (a < b))
+#Logical or operator-the result of the or statement will be true even if one of the statement evalute to true and other statement will evaluate to false
+print((a > b) or (a < b))
+#Logical not operator-the result of the program will be opposite of the result we obtain i.e if we get the value of a > b as true and the output value will be false and alse vice-versa
+print(not(a > b))
+#in the above statement the original output is true but since we are applying not operator we get the opposite of the true i.e false
