29eef76de493f58e41cd48ed965c52a40cf21404
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-03-30 22:16:38 +0530

    this code contains the if-elif-else

---
+++ if-elif-else.py
@@ -0,0 +1,9 @@
+#if elfi else - elif helps us to check multiple conditions in single if-else loop
+a = 10
+b = 11
+if(a>b):
+    print("A is greater than b")
+elif(b>a):
+    print("B is greater than A")
+else:
+    print("Both are equal")
