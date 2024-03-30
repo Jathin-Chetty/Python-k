46a0c1587956a0d5484d33edb7d38fa332272c5b
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-03-30 22:50:30 +0530

    this code contains the basics of for loop implementation'

---
+++ for-loop.py
@@ -0,0 +1,15 @@
+#for loop - it used for a range of numbers or character or anything 
+#for loop is the best used when we know the range of something
+#example let us consider the below code which prints the range of numbers inclusively
+for i in range(1,10):
+    print(i, end=' ')
+print("\n")
+#example let us consider the below code which prints the elements of the array 
+for i in [1,2,3,4,5]:
+    print(i, end =' ')
+print("\n")
+#end here is used to print all the elements in the same line
+for i in "hello":
+    print(i,end =' ')
+print("\n")
+# "\n" adds a new line in python 
