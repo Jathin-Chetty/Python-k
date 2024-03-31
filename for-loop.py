9e2d74388e6e153df7cb5d41bed929b25cd06807
Jathin Chetty <jathin.12221556@lpu.in>
2024-04-01 00:09:14 +0530

    added for each loop and use of underscore

--- for-loop.py
+++ for-loop.py
@@ -13,3 +13,23 @@ for i in "hello":
     print(i,end =' ')
 print("\n")
 # "\n" adds a new line in python 
+
+#for-else loop
+#in a for else loop the else statment will only run if the loop runs all its iterations
+#if the loop fails to run all its iterations due to a break statment the else statment will not be run
+for i in range(10):
+    print(i)
+else:
+    print("Finsihed running the loop")
+
+for i in range(10):
+    print(i)
+    if(i == 5): break
+else:
+    print("Finished running the loop")
+
+
+#using underscore in a for loop
+#if you are not using a variable initaited in the for loop you can use a underscore to not create memory for a variable
+for _ in range(5):
+    print("hello world")
