b04e7713ce6b1d1456243bd518aeafb32b96424d
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-03-31 20:46:56 +0530

    this code contains the basic implementation of while loop

---
+++ while.py
@@ -0,0 +1,7 @@
+#while loop - it is used to execute a peice of code repeatedly whenever the condition is true
+count = 0
+while (count < 5):
+    print("value is : ", count)
+    count+=1
+print("loop ended")
+#in the above code we are writing the condition that whenever count < 5 go inside the loop and execute it 
