7e38e8ea3fa589726f93ea39f4dc5b1fdd54f724
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-03-31 21:00:33 +0530

    this code contains how to implement the factorial of a number

---
+++ Factorial.py
@@ -0,0 +1,7 @@
+#factorial using for loop
+num = int(input("Enter the number: ")) #this is way we take dynamic input in python we declare the type of variable it is and then write the input keyword
+
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+print("factorial is:", factorial)
