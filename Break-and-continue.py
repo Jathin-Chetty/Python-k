3ddaecd00b89659c82774829b1592fd6efbc2691
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-04-01 23:23:40 +0530

    added Break and commit

--- Break-and-continue.py
+++ Break-and-continue.py
@@ -10,4 +10,4 @@ for i in range(10):
         continue
     print(i, end =' ')
 #output - 0 1 2 3 4 6 7 8 9
-#in the above 2 codes you can clearly see the result that the first code prints the values up until 5 and 2nd code prints all the values just skipping 5
+#in the above 2 codes you can clearly see the result that the first code prints the values up until 5 and 2nd code prints all the values just skipping 5.
