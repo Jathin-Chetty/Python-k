ff8d7eab31cfcd122286d697f1567186d6fe0a04
Kosalendra-Kalluri <120004955+Kosalendra-Kalluri@users.noreply.github.com>
2024-04-01 23:03:07 +0530

    this code contains the explanation about break and continue statement

---
+++ Break-and-continue.py
@@ -0,0 +1,13 @@
+#break statement - when ever the break statement is executed in a loop terminates and the next peice of code executes
+for i in range(10):
+    if(i==5):
+        break
+    print(i, end =' ')
+#output = 0 1 2 3
+#continue statement - when ever the continue statement is executed in a loop, the loop skips the present iteration and jumps to the next one and execute the remaining loop
+for i in range(10):
+    if(i==5):
+        continue
+    print(i, end =' ')
+#output - 0 1 2 3 4 6 7 8 9
+#in the above 2 codes you can clearly see the result that the first code prints the values up until 5 and 2nd code prints all the values just skipping 5
